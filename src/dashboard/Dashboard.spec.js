453d943afbdd37f25d9e9fb552778aaa91215953
Iyehvah <Iyehvah@users.noreply.github.com>
2020-01-19 08:44:23 -0600

    Dashboard is rendering properly and made a snapshot

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
+import React from "react";
+import Dashboard from "./Dashboard";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
 // Test away
+describe("Dashboard", () => {
+    it("Dashboard renders", () => {
+        const wrapper = render(<Dashboard/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
